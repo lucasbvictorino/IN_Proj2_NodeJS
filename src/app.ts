d745ec9b1e10dbc7fb4163e82086816709672778
Lucas Victorino <lucasvictorino@id.uff.br>
2026-02-24 14:08:27 -0300

    feat: Criando a primeira rota do usuário

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from './libs/prisma.js'
 
 export const app = fastify()
+
+app.post('/users', async (request, reply) => {
+    const registerUserBodySchema = z.object({
+        name: z.string().trim().min(1).max(100),
+        password: z.string().trim().min(8).max(100),
+        email: z.email().max(100),
+    })
+
+    const { name, password, email } = registerUserBodySchema.parse(request.body)
+
+    const user = await prisma.user.create({
+        data: {
+            name,
+            passwordHash: password,
+            email,
+        }
+    })
+
+    return reply.status(201).send(user)
+})
