c3f14e0f7d98651abecdc79ed29ee83a069fa123
Lucas Victorino <lucasvictorino@id.uff.br>
2026-05-05 17:51:30 -0300

    feat: Redis cache added

--- src/http/controller/posts/get-posts-by-user.controller.ts
+++ src/http/controller/posts/get-posts-by-user.controller.ts
@@ -1,5 +1,6 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
+import { redis } from '@/libs/redis.js'
 import { ResourceNotFoundError } from '@/use-cases/errors/resource-not-found-error.js'
 import { makeGetPostsByUser } from '@/use-cases/factories/make-get-posts-by-user.js'
 import { PostPresenter } from '../presenters/post-presenter.js'
@@ -14,12 +15,28 @@ export async function getPostsByUser(
     })
     const { userPublicId } = paramsSchema.parse(request.params)
 
+    const cacheKey = `posts:listByUser:${userPublicId}`
+    const cachedPosts = await redis.get(cacheKey)
+
+    if (cachedPosts) {
+      try {
+        const posts = JSON.parse(cachedPosts)
+        return reply.status(200).send({ posts })
+      } catch {
+        await redis.del(cacheKey)
+      }
+    }
+
     const useCase = makeGetPostsByUser()
     const { posts } = await useCase.execute({
       userPublicId,
     })
 
-    return reply.status(200).send({ posts: PostPresenter.toHTTP(posts) })
+    const presentedPosts = PostPresenter.toHTTP(posts)
+
+    await redis.set(cacheKey, JSON.stringify(presentedPosts), 'EX', 300)
+
+    return reply.status(200).send({ posts: presentedPosts })
   } catch (error) {
     if (error instanceof ResourceNotFoundError) {
       return reply.status(404).send({ error: error.message })
